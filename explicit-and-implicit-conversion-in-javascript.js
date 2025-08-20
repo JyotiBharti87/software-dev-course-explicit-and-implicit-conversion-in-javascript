6fd5c347372199d12197a4638a659a405ff1e96d
jyotibharti87 <joybharti87@gmail.com>
2025-08-20 18:03:42 -0500

    completed Practice 1: Values, Data Types, and Operations - 3 - Debugging Type Conversion

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,36 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = (Number("5")) - 2; //Previously "5" a String value. But I converted it to a numeric value using Number
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean("false"); // here the value is in String quotes "false", any value which is not 0, Null, undefined and empty string "" are truthy. which is why "false" is not FALSE
 if (isValid) {
     console.log("This is valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = (Number (age) + 5); //Previously age= "25"  was a String value. But I converted it to a numeric value using Number
 console.log("Total Age: " + totalAge);
+
+let Apples = "7.99";
+let grapes = "4.08" ;
+let totalValue = (parseFloat(Apples) + parseFloat(grapes)); //One example of explicit type conversion
+console.log (totalValue);
+
+//before implicit type conversion
+let total = "45" + 5;  
+console.log(total);   
+ 
+let momAge = "34";
+let dadAge = 40;
+let parentsAge = (momAge == dadAge); //implicit conversion of momAge String to a Number
+console.log (parentsAge);
+
+let Sun = true;
+if (Sun){
+  console.log("It's HOT today!")
+  if (null){
+    console.log("It's Truth") //null will not execute and falsy
+  }
+}
